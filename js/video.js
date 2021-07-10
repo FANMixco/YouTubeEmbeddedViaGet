6fd2ef1312b5ef99fb8199782e5ba0474795bd91
Federico Navarrete <fanmixco@gmail.com>
2021-07-10 21:59:52 +0200

    Added title

--- js/video.js
+++ js/video.js
@@ -1,7 +1,6 @@
 window.onload = function(){
     let url = new URL(window.location.href);
     let searchParams = new URLSearchParams(url.search);
-    let video = searchParams.get('video');
-
-    document.getElementById('youtube').src = `https://www.youtube.com/embed/${video}`;
+    document.getElementById('youtube').src = `https://www.youtube.com/embed/${searchParams.get('video')}`;
+    document.title = searchParams.get('title');
 }
